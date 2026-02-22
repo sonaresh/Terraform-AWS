c51c3452ab39270c8a1335abf1a691cf7fd5b2b3
Naresh S <95885317+sonaresh@users.noreply.github.com>
2026-02-21 21:28:48 -0500

    Delete provider.tf

--- provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  alias  = "Prod"
-  region = var.region
-  profile = "terraformuser"
-  assume_role {
-    role_arn = "arn:aws:iam::<AccountID>:role/TerraformProvisioningRole"
-  }
-}
